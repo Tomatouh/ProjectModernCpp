3328df8a28f9cb3535f6480feeea69b40d9c7eba
KeldeeoR <vasilica.lalu@student.unitbv.ro>
2025-10-29 12:01:15 +0200

    Implemented basic Player class

---
+++ ProjectModernCpp/ProjectModernCpp/Player.cpp
@@ -0,0 +1,4 @@
+#include "Player.h"
+
+Player::Player(const std::string& n)
+	: name(n), score(0) {}
---
+++ ProjectModernCpp/ProjectModernCpp/Player.h
@@ -0,0 +1,12 @@
+#pragma once
+#include<string>
+
+class Player
+{
+public:
+	std::string name;
+	int score;
+
+	Player(const std::string& n);
+};
+
