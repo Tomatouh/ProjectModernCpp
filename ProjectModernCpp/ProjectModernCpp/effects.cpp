bc1d7391462dfaca50e236151473dd969f9e4495
GheorgheDavid21 <david-d.gheorghe@student.unitbv.ro>
2025-11-19 11:19:54 +0200

    added some effects

--- ProjectModernCpp/ProjectModernCpp/effects.cpp
+++ ProjectModernCpp/ProjectModernCpp/effects.cpp
@@ -38,9 +38,56 @@ void addResources(Player& player, const Building& building)
 	}
 }
 
+//void addEveryResource(Player& player, const Building& card)
+//{
+//	for (auto& res : card.getResources())
+//	{
+//		switch (res)
+//		{
+//			case ResourceType::WOOD
+//		}
+//	}
+//}
+
 void addScientificSymbol(Player& player, const Building& card)
 {
 	player.addBuilding(card);
+}
+
+void threeCoinsPerGray(Player& player)
+{
+	player.addCoins(player.getGreyBuildings().size() * 3);
+}
+
+void twoCoinsPerBrown(Player& player)
+{
+	player.addCoins(player.getBrownBuildings().size() * 2);
+}
+
+void oneCoinPerYellow(Player& player)
+{
+	player.addCoins(player.getYellowBuildings().size());
+}
+
+void oneCoinPerRed(Player& player)
+{
+	player.addCoins(player.getRedBuildings().size());
+}
+
+void tradersGuild(Player& player1, Player& player2)
+{
+	player1.addCoins(std::max(player1.getYellowBuildings().size(), player2.getYellowBuildings().size()));
+
+}
+
+void loseThreeCoins(Player& player, Bank& bank)
+{
+	player.spendCoins(3);
+}
+
+void discardGrey(Player& player, std::uint8_t idCard)
+{
+	//player.discard(grey_cards, idCard);
 }
 
 //void addShields(bool player, Board& board)
