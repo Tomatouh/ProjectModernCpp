acaacb9d2487fb8e583e97dcfd18e1edf358ee46
lfalex-creator <florin.lupu@student.unitbv.ro>
2025-12-30 15:25:01 +0200

    Updated the environment to support SFML

--- ProjectModernCpp/ProjectModernCpp/main.cpp
+++ ProjectModernCpp/ProjectModernCpp/main.cpp
@@ -1,3 +1,4 @@
+#include <SFML/Graphics.hpp>
 import game;
 import Player;
 //import cardsLoader;
@@ -20,7 +21,23 @@ import <vector>;
 */
 int main() {
 
-	Game game;
-	game.run();
+	/*Game game;
+	game.run();*/
+    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 	return 0;
 }
