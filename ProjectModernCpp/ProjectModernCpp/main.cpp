562deecd509cb083f24f2332f9b90bea7cc3a9e8
lfalex-creator <florin.lupu@student.unitbv.ro>
2025-12-31 19:08:13 +0200

    Ignore

--- ProjectModernCpp/ProjectModernCpp/main.cpp
+++ ProjectModernCpp/ProjectModernCpp/main.cpp
@@ -1,4 +1,3 @@
-#include <SFML/Graphics.hpp>
 import game;
 import Player;
 //import cardsLoader;
@@ -21,23 +20,7 @@ import <vector>;
 */
 int main() {
 
-	/*Game game;
-	game.run();*/
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+	Game game;
+	game.run();
 	return 0;
 }
