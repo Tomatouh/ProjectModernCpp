aa4a26849449637dc31f1d4f9cc1ab0c3b42daae
Damian Albrich <damian.albrich@student.unitbv.ro>
2026-01-07 16:18:06 +0200

    changed structure, finished cropping wonders. added TODO

--- ProjectModernCpp/ProjectModernCpp/main.cpp
+++ ProjectModernCpp/ProjectModernCpp/main.cpp
@@ -9,6 +9,19 @@ import <vector>;
 //import <memory>;
 
 /*
+* TODO: fix the trade function for compatibility with everything
+		save function: 
+				-individual card vectors from player, whose turn is it, current game state-presume ONGOING, make sure it checks it's ongoing before saving.
+				-victory points for each pl;ayer, each player's data is saved separately, player 1 first, military score and peon position, whether or not certain zones were triggered
+				-progress tokens still available to get
+				-each player's progress tokens, each player's scientific symbols
+				-discarded progress tokens
+				-money each player has
+				-production for each player
+				-discard pile vector
+				-current card layout+randomize the decks for the future ages
+				-age
+				-wonder progress, whether or not it is built, which card back is showing behind it
 * ID's : brown cards 1-9(1-3 wood, 4-6 clay, 7-9 stone)
 *		 gray cards 10-13(10-11 glass, 12-13 papyrus)
 *        red cards 14-27(14-19 1 shield, additionally grouped by linking, 20-25 2 shields, additionally grouped by linking, 26-27 3 shields)
