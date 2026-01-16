4a0449d2cc94c819c5cba354485beca05d55fda8
lfalex-creator <florin.lupu@student.unitbv.ro>
2026-01-16 20:43:29 +0200

    Removed scale symbol from player details Players can now select progress tokens.

--- ProjectModernCpp/ProjectModernCpp/game.cpp
+++ ProjectModernCpp/ProjectModernCpp/game.cpp
@@ -1379,6 +1379,26 @@ void Game::drawConstructionChoices(sf::RenderWindow& window)
 	box.drawOptions(window);
 }
 
+bool Game::selectToken(sf::RenderWindow& window, const sf::Vector2i& mousePos)
+{
+	sf::Vector2f worldPos = window.mapPixelToCoords(mousePos);
+	const float xPosStart = static_cast<float>(window.getSize().x) / 2.f - (m_progressTokensDeck.size() * 148.f) / 2.f;
+	const float yPos = static_cast<float>(window.getSize().y) / 2.f - 75.f;
+	const float tokenBoxWidth = 100.f;
+	const float tokenBoxHeight = 80.f;
+	for (int i = 0; i < m_progressTokensDeck.size(); ++i) {
+		sf::FloatRect tokenRect(
+			sf::Vector2f(xPosStart + i * 154, yPos),
+			sf::Vector2f(tokenBoxWidth, tokenBoxHeight)
+		);
+		if (tokenRect.contains(worldPos)) {
+			m_currentPlayer->addProgressToken(*(m_progressTokensDeck[i]));
+			m_progressTokensDeck.erase(m_progressTokensDeck.begin() + i);
+			return true;
+		}
+	}
+	return false;
+}
 int Game::getConstructionOption(sf::RenderWindow& window, const sf::Vector2i& mousePos)
 {
 	sf::Vector2f worldPos = window.mapPixelToCoords(mousePos);
@@ -1421,6 +1441,15 @@ int Game::chooseConstructionOption(sf::RenderWindow& window, const sf::Vector2i&
 				window.draw(m_backgroundSprite);
 				drawTokenSelection(window);
 				window.display();
+				while(const std::optional event = window.waitEvent())
+				{
+					if(event->is<sf::Event::MouseButtonPressed>())
+					{
+						sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+						if (selectToken(window, mousePos))
+							break;
+					}
+				}
 			}
 			std::swap(m_currentPlayer, m_otherPlayer);
 
@@ -1798,17 +1827,11 @@ void Game::drawPlayerBox(sf::RenderWindow& window,const std::shared_ptr<Player>&
 	triangleSprite.setScale({ 0.3f, 0.3f });
 	triangleSprite.setPosition({ backgroundBox.getPosition().x + 20.f, backgroundBox.getPosition().y + 500.f });
 
-	sf::Texture scaleTexture;
-	scaleTexture.loadFromFile("..\\..\\Images\\Scientific Symbols\\triangle.png");
-	sf::Sprite scaleSprite(triangleTexture);
-	scaleSprite.setScale({ 0.3f, 0.3f });
-	scaleSprite.setPosition({ backgroundBox.getPosition().x + 20.f, backgroundBox.getPosition().y + 550.f });
-
 	sf::Texture wheelTexture;
 	wheelTexture.loadFromFile("..\\..\\Images\\Scientific Symbols\\wheel.png");
 	sf::Sprite wheelSprite(wheelTexture);
 	wheelSprite.setScale({ 0.3f, 0.3f });
-	wheelSprite.setPosition({ backgroundBox.getPosition().x + 20.f, backgroundBox.getPosition().y + 600.f });
+	wheelSprite.setPosition({ backgroundBox.getPosition().x + 20.f, backgroundBox.getPosition().y + 550.f });
 
 	sf::Text globeLabel(font, ": " + std::to_string(selectedPlayer->getScientificPoints()[0]), 20);
 	globeLabel.setPosition({ backgroundBox.getPosition().x + 60.f, backgroundBox.getPosition().y + 307.f });
@@ -1830,12 +1853,8 @@ void Game::drawPlayerBox(sf::RenderWindow& window,const std::shared_ptr<Player>&
 	triangleLabel.setPosition({ backgroundBox.getPosition().x + 60.f, backgroundBox.getPosition().y + 507.f });
 	triangleLabel.setFillColor(sf::Color::Black);
 
-	sf::Text scaleLabel(font, ": " + std::to_string(selectedPlayer->getScientificPoints()[1]), 20);
-	scaleLabel.setPosition({ backgroundBox.getPosition().x + 60.f, backgroundBox.getPosition().y + 557.f });
-	scaleLabel.setFillColor(sf::Color::Black);
-
 	sf::Text wheelLabel(font, ": " + std::to_string(selectedPlayer->getScientificPoints()[6]), 20);
-	wheelLabel.setPosition({ backgroundBox.getPosition().x + 60.f, backgroundBox.getPosition().y + 607.f });
+	wheelLabel.setPosition({ backgroundBox.getPosition().x + 60.f, backgroundBox.getPosition().y + 557.f });
 	wheelLabel.setFillColor(sf::Color::Black);
 
 	sf::RectangleShape exitBox;
@@ -1863,13 +1882,11 @@ void Game::drawPlayerBox(sf::RenderWindow& window,const std::shared_ptr<Player>&
 	window.draw(sundialSprite);
 	window.draw(triangleSprite);
 	window.draw(wheelSprite);
-	window.draw(scaleSprite);
 	window.draw(globeLabel);
 	window.draw(featherLabel);
 	window.draw(mortarLabel);
 	window.draw(sundialLabel);
 	window.draw(triangleLabel);
-	window.draw(scaleLabel);
 	window.draw(wheelLabel);
 	window.draw(exitBox);
 	window.draw(exitLabel);
@@ -1949,14 +1966,15 @@ void Game::drawMilitaryBoard(sf::RenderWindow& window)
 
 void Game::drawTokenSelection(sf::RenderWindow& window)
 {
-	int x = static_cast<float>(window.getSize().x) / 2.f - (m_progressTokensDeck.size() * 148.f) / 2.f;
+	float x = static_cast<float>(window.getSize().x) / 2.f - (m_progressTokensDeck.size() * 148.f) / 2.f;
+	float y = static_cast<float>(window.getSize().y) / 2.f - 75.f;
 	for(auto& token : m_progressTokensDeck)
 	{
 		sf::Texture tokenTexture;
 		tokenTexture.loadFromFile("..\\..\\Images\\Progress tokens\\" + std::to_string(token->getId()) + ".png");
 		sf::Sprite tokenSprite(tokenTexture);
 		tokenSprite.setScale({ 0.4f, 0.4f });
-		tokenSprite.setPosition({ static_cast<float>(x), static_cast<float>(window.getSize().y) / 2.f - 75.f });
+		tokenSprite.setPosition({ x, y });
 		x += 154;
 		window.draw(tokenSprite);
 	}
